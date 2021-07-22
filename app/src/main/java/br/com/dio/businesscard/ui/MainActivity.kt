ee14dff3eb61f242761a27ed536d0c8c681af473
wesle_p1v78x7 <wesleyorrr@gmail.com>
2021-07-22 12:08:32 -0300

    App ultilizando essas Tecnologias Room, MVVM, ViewBinding, LiveData, ListAdapter, Design.

---
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package br.com.dio.businesscard.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import br.com.dio.businesscard.App
+
+
+import br.com.dio.businesscard.databinding.ActivityMainBinding
+import br.com.dio.businesscard.ultil.Image
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy { BusinessCardAdapter() }
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+            binding.rvCards.adapter = adapter
+            getAllBusinessCard()
+            InsertListenes()
+    }
+ private fun InsertListenes(){
+     binding.fab.setOnClickListener {
+        val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+
+         startActivity(intent)
+     }
+     adapter.listenerShare = { card ->
+         Image.share(this@MainActivity, card)
+     }
+ }
+    private fun getAllBusinessCard(){
+
+        mainViewModel.getAll().observe(this,{businessCards ->
+            adapter.submitList(businessCards)
+        })
+    }
+
+ }
+
